c2652d522f16ab09f109b502945847e62c6cf599
Omri <omrid3103@gmail.com>
2024-01-19 18:14:14 +0200

    complete sign-up and sign-in

--- sign_authentication.py
+++ sign_authentication.py
@@ -1,7 +1,12 @@
 import sqlalchemy
 from sqlalchemy.orm.session import sessionmaker
 import hashlib
+import uvicorn
+from fastapi import FastAPI
+from validate_email import validate_email
 
+
+app = FastAPI()
 engine = sqlalchemy.create_engine('sqlite:///datacamp.sqlite')
 conn = engine.connect()
 metadata = sqlalchemy.MetaData()
@@ -19,7 +24,7 @@ Authentication = sqlalchemy.Table('Authentication', metadata,
                                   )
 
 session = Session()
-# metadata.create_all(engine)
+metadata.create_all(engine)
 
 # query = sqlalchemy.insert(HumanResources).values(DepartmentID=17, Name='Mathematics', GroupName='math')
 # session.execute(query)
@@ -43,56 +48,54 @@ session = Session()
 # json_Result = session.execute(query)
 # print(json_Result.fetchall())
 
+
+"""Completed sign-in method"""
+
+
+@app.get("/authenticate")
+def authenticate(email: str, username: str, password: str) -> dict:
+    user_auth = Authentication.select().where(Authentication.columns.Username == username)
+    user_auth = session.execute(user_auth).fetchall()
+    user_not_exist: bool = user_auth == []
+
+    if user_not_exist:
+        return {'response': "Username doesnt exist!", "authentication": False}
+
+    if not any(data[-3] == email for data in user_auth):
+        return {'response': "Invalid email!", "authentication": False}
+
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    if not any(data[-1] == hashed_password for data in user_auth):
+        return {'response': "Not matching password!", "authentication": False}
+
+    return {'response': "Signing in...", "authentication": True}
+
+
 """Completed sign-up func"""
 
 
-def sign_up(email: str, username: str, password: str):
+@app.get("/sign_up")
+def sign_up(email: str, username: str, password: str) -> dict:
     user_auth = Authentication.select().where(Authentication.columns.Username == username)
     user_auth = session.execute(user_auth).fetchall()
     username_query: bool = user_auth == []
-    session.commit()
-    if username_query:
-        pass_hash = hashlib.sha256(password.encode()).hexdigest()
-        email_auth = Authentication.select().where(Authentication.columns.Email == email)
-        email_auth = session.execute(email_auth).fetchall()
-        email_query: bool = email_auth == []
-        if not email_query:
-            return "There is an account with the same email already!"
-            # add in flet a way to allow rewriting the accounts details and rerunning the function
-        else:
-            insert_query = sqlalchemy.insert(Authentication).values(Email=email, Username=username, Password=pass_hash)
-            session.execute(insert_query)
-            session.commit()
-        return "Signed up successfully!"
-    else:
-        sign_in(email, username, password)
-        return None
-
-
-"""Completed sign in func"""
-
-
-def sign_in(email: str, username: str, password: str):
-    user_auth = Authentication.select().where(Authentication.columns.Username == username)
-    user_auth = session.execute(user_auth).fetchall()
-    username_query: bool = user_auth == []
-    session.commit()
     if not username_query:
-        pass_hash = hashlib.sha256(password.encode()).hexdigest()
-        query_details_list = Authentication.select().where(Authentication.columns.Username == username)
-        username_details_list = session.execute(query_details_list).fetchall()
-        email_query = username_details_list[0][1]
-        pass_query = username_details_list[0][3]
-        if email_query == email:
-            if pass_query == pass_hash:
-                return "Signed in successfully!"
-            else:
-                return "Wrong password!"
-        else:
-            return "Wrong email address!"
+        return {"response": "Username taken! Try signing in..."}
+    pass_hash = hashlib.sha256(password.encode()).hexdigest()
+    if not validate_email(email):
+        return {"response": "invalid email address!"}
+    email_auth = Authentication.select().where(Authentication.columns.Email == email)
+    email_auth = session.execute(email_auth).fetchall()
+    email_query: bool = email_auth == []
+    if not email_query:
+        return {"response": "There is an account with the same email already!"}
+        # add in flet a way to allow rewriting the accounts details and rerunning the function
     else:
-        sign_up(email, username, password)
-        return None
+        insert_query = sqlalchemy.insert(Authentication).values(Email=email, Username=username, Password=pass_hash)
+        session.execute(insert_query)
+        session.commit()
+        return {"response": "Signed up successfully!"}
+
 
 
 def main():
@@ -105,16 +108,7 @@ def main():
     # print(sign_in('omrid31@gmail.com', 'pOmri1', 'oiedvdi'))
     """if it doesnt work try: ctrl ? on line 22"""
 
+
 if __name__ == '__main__':
-    main()
-
-
-
-
-
-
-
-
-
-
-
+    # main()
+    uvicorn.run(app, host="127.0.0.1", port=5555)
