33b58f3fec48ec0eb2b0e44821d926f74b3623df
Omri <omrid3103@gmail.com>
2024-02-08 21:42:50 +0200

    kmc with numpy

--- kmeans_clustering.py
+++ kmeans_clustering.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 x = [[4.0, 10.0], [4.1, 11.0], [3.8, 9.0], [3.9, 9.8], [4.0, 10.5], [5.0, 3.0], [4.8, 5.0], [5.2, 4.0], [5.1, 2.0],
      [4.9, 3.0]]
 
+npa = np.array(x)
+
 
 # center_list = [random.randrange(1, 10), random.randrange(1, 10), random.randrange(1, 10)]
 # # second_center = [random.randrange(1, 10), random.randrange(1, 10), random.randrange(1, 10)]
@@ -33,37 +35,60 @@ x = [[4.0, 10.0], [4.1, 11.0], [3.8, 9.0], [3.9, 9.8], [4.0, 10.5], [5.0, 3.0], 
 # print(str(list_first) + "\n\n\n\n\n\n")
 # print(str(list_second))
 
+def distance_calc(x_point: float, y_point: float, x_center: float, y_center: float) -> float:
+    return math.sqrt(math.pow(x_point - x_center, 2) + math.pow(y_point - y_center, 2))
 
-def point_distance(point: list[float], centers_list: list[list[float]]):
+
+def point_distance(x_point: float, y_point: float, centers_array: np.array):
+    #centers_list: list[list[float]]
     """dictionary that keeps a tuple of two points (the arg point and the current center from the arg centers_list)"""
-    dist_dict: dict[tuple[tuple[float], tuple[float]], float] = {}
+    # dist_dict: dict[tuple[tuple[float], tuple[float]], float] = {}
+    distance_arr = np.zeros([len(centers_array), 3], dtype=float)
 
     """adds to the latter dictionary a cell of: key - the tuple of the point and current center (c), value -the wanted distance between 
             the current center and point"""
-    for c in centers_list:
-        dist_dict[(tuple(point), tuple(c))] = math.dist(point, c)
+    i = 0
+    for c in centers_array:
+        row_list: list[float, float, float] = [x_point, y_point, distance_calc(x_point, y_point, c[0], c[1])]
+        row_array = np.array(row_list)
+        distance_arr[i, :] = row_array
+        i += 1
+        # dist_dict[(tuple(point), tuple(c))] = math.dist(point, c)
 
     """keeps in a variable the minimal distance between the point and the right center"""
-    min_dist = min(dist_dict.values())
+    min_dist, min_dist_index = distance_arr[:, 2].min(), distance_arr[:, 2].argmin()
+    return min_dist_index
+    # """return the index of the min-distance center in the dictionary"""
+    # index = 0
+    # for c in centers_list:
+    #     if min_dist == dist_dict[(tuple(point), tuple(c))]:
+    #         return index
+    #     index += 1
 
-    """return the index of the min-distance center in the dictionary"""
-    index = 0
-    for c in centers_list:
-        if min_dist == dist_dict[(tuple(point), tuple(c))]:
-            return index
-        index += 1
 
-
-def kmc(x: list[list[float]], n: int):
-    centers_list: list[list[float]] = []
+def kmc(points_array: np.array, n: int):
+    centers_array: np.array = np.zeros([n, 2], dtype=float)
     index_dict: dict[int, list[list[float]]] = {}
+    grouping_array = np.zeros([len(points_array), 3])
     for i in range(n):
-        center_var: list[float] = [random.randrange(1, 10), random.randrange(1, 10)]
-        centers_list.append(center_var)
+        specific_center_arr: np.array = np.array([random.randrange(1, 10), random.randrange(1, 10)])
+        centers_array[i, :] = specific_center_arr
         index_dict[i] = []
     # for i in range(10):
-    for p in x:
-        index_dict[point_distance(p, centers_list)].append(p)
+    i = 0
+    for p in points_array:
+        # index_dict[point_distance(p, centers_list)].append(p)
+        grouping_array[i, :] = np.array([point_distance(p[0], p[1], centers_array), p[0], p[1]])
+        i += 1
         # for l in range(len(centers_list)):
         #     centers_list[l] = [random.randrange(1, 10), random.randrange(1, 10)]
-    return index_dict, centers_list
+    return grouping_array, centers_array
+
+
+def main():
+    g_a, c_a = kmc(npa, 4)
+    print(g_a)
+    print(c_a)
+
+if __name__ == "__main__":
+    main()
--- scatter_graph.py
+++ scatter_graph.py
@@ -1,30 +1,74 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
+from main import kmc
 from dataclasses import dataclass
+
 
 # x = np.array()
 # plt.scatter(x[ : , 0], x[ :, 1], s = 100, c = 'b')
 # plt.show()
+class Point:
+    def __init__(self, x: float, y: float):
+        self.x: float = x
+        self.y: float = y
 
-@dataclass(slots=True, frozen=False)
-class Point():
-    x: int
-    y: int
+
+# points = [Point(4.0, 10.0), Point(4.1, 11.0), Point(3.8, 9.0), Point(3.9, 9.8), Point(4.0, 10.5), Point(5.0, 3.0), Point(4.8, 5.0), Point(5.2, 4.0), Point(5.1, 2.0), Point(4.9, 3.0)]
+# points = [[4.0, 10.0, 8.0], [4.1, 11.0, 7.0], [3.8, 9.0, 9.0], [3.9, 9.8, 8.0], [4.0, 10.5, 7.9], [5.0, 3.0, 4.0], [4.8, 5.0, 5.0], [5.2, 4.0, 4.5], [5.1, 2.0, 4.2], [4.9, 3.0, 3.7]]
+points = [
+    [4.0, 10.0],
+    [4.1, 11.0],
+    [3.8, 9.0],
+    [3.9, 9.8],
+    [4.0, 10.5],
+    [5.0, 3.0],
+    [4.8, 5.0],
+    [5.2, 4.0],
+    [5.1, 2.0],
+    [4.9, 3.0],
+    [4.4, 7.9],
+    [5.1, 10.5],
+    [4.8, 10.1],
+    [5.2, 8.5],
+    [4.1, 5.6],
+    [4.4, 7.6],
+    [3.9, 4.9]
+]
+dict1, centers = kmc(points, 4)
+list1 = dict1[0]
+print(list1)
+list2 = dict1[1]
+print(list2)
+list3 = dict1[2]
+print(list3)
+list4 = dict1[3]
+print(list4)
 
 
-points = list()
-for i in range(14):
-    x = i
-    y = pow(i, 2)
-    p = Point(x, y)
-    points.append(p)
+
+
 
 # The question starts here.
 # The best I could come up with so far:
-x = np.fromiter((p.x for p in points), int)
-y = np.fromiter((p.y for p in points), int)
-arr = np.vstack((x, y)).transpose()
-print(arr)
-plt.scatter(arr[ : , 0], arr[ :, 1], s = 13, c = 'g')
+
+x = np.fromiter((l1[0] for l1 in list1), float)
+y = np.fromiter((l1[1] for l1 in list1), float)
+arr1 = np.vstack((x, y)).transpose()
+x = np.fromiter((l2[0] for l2 in list2), float)
+y = np.fromiter((l2[1] for l2 in list2), float)
+arr2 = np.vstack((x, y)).transpose()
+x = np.fromiter((l3[0] for l3 in list3), float)
+y = np.fromiter((l3[1] for l3 in list3), float)
+arr3 = np.vstack((x, y)).transpose()
+x = np.fromiter((l4[0] for l4 in list4), float)
+y = np.fromiter((l4[1] for l4 in list4), float)
+arr4 = np.vstack((x, y)).transpose()
+x = np.fromiter((l5[0] for l5 in centers), float)
+y = np.fromiter((l5[1] for l5 in centers), float)
+arr5 = np.vstack((x, y)).transpose()
+plt.scatter(arr1[:, 0], arr1[:, 1], s=13, c='g')
+plt.scatter(arr2[:, 0], arr2[:, 1], s=13, c='r')
+plt.scatter(arr3[:, 0], arr3[:, 1], s=13, c='b')
+plt.scatter(arr4[:, 0], arr4[:, 1], s=13, c='y')
+plt.scatter(arr5[:, 0], arr5[:, 1], s=25, c='black')
 plt.show()
