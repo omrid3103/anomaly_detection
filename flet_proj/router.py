f6169f6a1d6bb99cc35da2b2d734546f60fc7ea1
omrid3103 <150009701+omrid3103@users.noreply.github.com>
2024-03-08 11:54:30 +0200

    Update router.py

--- flet_proj/router.py
+++ flet_proj/router.py
@@ -3,20 +3,25 @@ from flet_proj.home import home_view
 from flet_proj.sign_up_flet import sign_up_view
 from flet_proj.Index import index_view
 from flet_proj.authentication_flet import authentication_view
+from flet_proj.user_landing_page import user_landing_view
 
 
 class Router:
 
     def __init__(self, page):
+
+        self.signup_controller = sign_up_view(page, self)
+        self.signin_controller = authentication_view(page, self)
         self.page = page
         self.routes = {
-            "/index": index_view(page),
-            "/home": home_view(page),
-            "/sign_up_flet": sign_up_view(page),
-            "/authentication_flet": authentication_view(page)
+            "/index": index_view(page, self),
+            "/home": home_view(page, self),
+            "/sign_up_flet": self.signup_controller.column,
+            "/authentication_flet": self.signin_controller.column,
+            "/user_landing_page": user_landing_view(page, self)
         }
         self.body = ft.Container(content=self.routes["/home"])
 
     def route_change(self, route):
-        self.body.content = self.routes[route.route ]
+        self.body.content = self.routes[route.route]
         self.body.update()
