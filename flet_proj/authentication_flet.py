ed04fede527cba9eb216a5c62b572d8e584d0f63
omrid3103 <150009701+omrid3103@users.noreply.github.com>
2024-03-11 11:43:12 +0200

    Update authentication_flet.py

--- flet_proj/authentication_flet.py
+++ flet_proj/authentication_flet.py
@@ -5,19 +5,18 @@ from typing import Union
 
 
 class Authentication:
-    def __init__(self, page, router):
+    def __init__(self, page):
         self.details: dict[str, str] = {"username": "", "email": ""}
-        self.router = router
         self.page = page
         self.username_tb = ft.TextField(label="Username", max_lines=1, width=280, hint_text="Enter username here")
         self.email_tb = ft.TextField(label="Email", max_lines=1, width=280, hint_text="Enter email here")
         self.password_tb = ft.TextField(label="Password", password=True, can_reveal_password=True, max_lines=1, width=280, hint_text="Enter password here")
-        self.submit_button = ft.ElevatedButton(text="Sign Me In!", on_click=self.submit_button_clicked)
+        self.submit_button = ft.ElevatedButton()
         self.items = [self.username_tb, self.email_tb, self.password_tb, self.submit_button]
         self.column = ft.Column(spacing=20, controls=self.items)
         # example_tb2 = ft.TextField(label="Disabled", disabled=True, read_only=True, hint_text="Please enter text here", icon=ft.icons.EMOJI_EMOTIONS, value="First name")
 
-    def submit_button_clicked(self, e):
+    def sign_in_button_clicked(self, e):
         flag = True
         if self.username_tb.value == "":
             self.username_tb.border_color = ft.colors.RED_400
@@ -82,7 +81,50 @@ class Authentication:
                 time.sleep(1)
                 self.page.go('/user_landing_page')
         self.page.update()
+
         # redirect to another page
+
+    def sign_up_button_clicked(self, e):
+        result = requests.get("http://127.0.0.1:5555/sign_up",
+                              params={"email": self.email_tb.value, "username": self.username_tb.value,
+                                      "password": self.password_tb.value}).json()
+        user_information = {"email": self.email_tb.value, "username": self.username_tb.value, "password": self.password_tb.value}
+        # result = sa.sign_up(self.email_tb.value, self.username_tb.value, self.password_tb.value)
+        response = result["response"]
+        print(response)
+        if response == "Username invalid!":
+            self.username_tb.border_color = ft.colors.RED_400
+            self.username_tb.value = ''
+            self.username_tb.label = response
+            self.username_tb.hint_text = "Enter new username here"
+            self.email_tb.border_color = ft.colors.SURFACE_VARIANT
+            self.password_tb.border_color = ft.colors.SURFACE_VARIANT
+            self.email_tb.label = "Email"
+            self.password_tb.label = "Password"
+        if response == "Invalid email address!" or response == "Account with the same email exists!":
+            self.email_tb.border_color = ft.colors.RED_400
+            self.email_tb.value = ''
+            self.email_tb.label = response
+            self.email_tb.hint_text = "Enter new email here"
+            self.username_tb.border_color = ft.colors.SURFACE_VARIANT
+            self.password_tb.border_color = ft.colors.SURFACE_VARIANT
+            self.username_tb.label = "Username"
+            self.password_tb.label = "Password"
+        if response == "Invalid password!":
+            self.password_tb.border_color = ft.colors.RED_400
+            self.password_tb.value = ''
+            self.password_tb.label = response
+            self.password_tb.hint_text = "Enter new password here"
+            self.username_tb.border_color = ft.colors.SURFACE_VARIANT
+            self.email_tb.border_color = ft.colors.SURFACE_VARIANT
+            self.username_tb.label = "Username"
+            self.email_tb.label = "Email"
+        self.page.update()
+        if response == "Signed up successfully!":
+            self.details["username"] = self.username_tb.value
+            self.details["email"] = self.email_tb.value
+            time.sleep(1)
+            self.page.go('/user_landing_page')
 
     def main(self) -> None:
         # page.scroll = ft.ScrollMode.HIDDEN
@@ -94,7 +136,7 @@ class Authentication:
 
 
 def authentication_view(page: ft.Page, router):
-    return Authentication(page, router)
+    return Authentication(page)
 
 
 
