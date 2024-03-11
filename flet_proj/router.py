e9d638ec1468c4e6955d6edbca5f19621095b625
omrid3103 <150009701+omrid3103@users.noreply.github.com>
2024-03-11 11:43:38 +0200

    Update router.py

--- flet_proj/router.py
+++ flet_proj/router.py
@@ -1,24 +1,39 @@
 import flet as ft
-from flet_proj.home import home_view
-from flet_proj.sign_up_flet import sign_up_view
-from flet_proj.Index import index_view
 from flet_proj.authentication_flet import authentication_view
 from flet_proj.user_landing_page import user_landing_view
+from flet_proj.initial_page import initial_page_view
 
 
 class Router:
 
     def __init__(self, page):
-
-        self.signup_controller = sign_up_view(page, self)
-        self.signin_controller = authentication_view(page, self)
         self.page = page
+        self.sign_up_controller = authentication_view(self.page, self)
+        self.sign_up_controller.submit_button.on_click = self.sign_up_controller.sign_up_button_clicked
+        self.sign_up_controller.submit_button.text = "Sign Me Up!"
+
+        self.sign_in_controller = authentication_view(self.page, self)
+        self.sign_in_controller.submit_button.on_click = self.sign_in_controller.sign_in_button_clicked
+        self.sign_in_controller.submit_button.text = "Sign Me In!"
+
+        self.index = initial_page_view(self.page)
+        self.home = initial_page_view(self.page)
+
+        self.user_landing = user_landing_view(self.page)
+        if self.sign_up_controller.details["username"] == "":
+            self.user_landing.details = self.sign_in_controller.details
+
+        else:
+            self.user_landing.details = self.sign_up_controller.details
+
+        self.user_landing.update_text()
+
         self.routes = {
-            "/index": index_view(page, self),
-            "/home": home_view(page, self),
-            "/sign_up_flet": self.signup_controller.column,
-            "/authentication_flet": self.signin_controller.column,
-            "/user_landing_page": user_landing_view(page, self)
+            "/index": self.index.content,
+            "/home": self.home.content,
+            "/sign_up_flet": self.sign_up_controller.column,
+            "/authentication_flet": self.sign_in_controller.column,
+            "/user_landing_page": self.user_landing.content
         }
         self.body = ft.Container(content=self.routes["/home"])
 
