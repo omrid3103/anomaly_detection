7ad17f62d976eb502b60d89b1c3a2d4ca419ff97
omrid3103 <150009701+omrid3103@users.noreply.github.com>
2024-03-15 11:41:03 +0200

    Update router.py

--- flet_proj/router.py
+++ flet_proj/router.py
@@ -21,17 +21,22 @@ class Router:
 
         self.user_landing = user_landing_view(self.page)
 
+        self.user_action = user_landing_view(self.page)
+        self.user_action.title_text = "ACTION"
+
         self.routes = {
             "/index": self.index.content,
             "/home": self.home.content,
             "/sign_up_flet": self.sign_up_controller.column,
             "/authentication_flet": self.sign_in_controller.column,
-            "/user_landing_page": self.user_landing.content
+            "/user_landing_page": self.user_landing.content,
+            "/user_action_page": self.user_action.content,
         }
         self.body = ft.Container(content=self.routes["/home"])
 
     def update_credentials(self) -> None:
 
+        print(self.user_landing.details)
         if self.sign_in_controller.details["username"] == "":
             self.sign_up_controller.update_details()
             self.user_landing.details["username"] = self.sign_up_controller.details["username"]
@@ -43,6 +48,7 @@ class Router:
             self.user_landing.details["email"] = self.sign_in_controller.details["email"]
 
         self.user_landing.update_text()
+        print(self.user_landing.details)
 
     def route_change(self, route):
         self.body.content = self.routes[route.route]
