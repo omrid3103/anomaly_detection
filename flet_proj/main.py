e96c38690ac0e25c0608e502ef4f0ae3006b6b5f
omrid3103 <150009701+omrid3103@users.noreply.github.com>
2024-03-15 11:40:50 +0200

    Update main.py

--- flet_proj/main.py
+++ flet_proj/main.py
@@ -1,15 +1,19 @@
 import flet as ft
+from flet_proj.appbar import AppBar
 
 from flet_proj.router import Router
-from flet_proj.appbar import appbar
+from flet_proj.appbar import AppBar
 
 
 def main(page: ft.Page):
 
     page.theme_mode = "light"
 
-    page.appbar = AppBar(page).my_appbar
     my_router = Router(page)
+    temp_appbar = AppBar(page)
+    if my_router.user_landing.details["username"] != "":
+        temp_appbar.update_menu()
+    page.appbar = temp_appbar.my_appbar
 
     page.on_route_change = my_router.route_change
 
