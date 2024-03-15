1f3a00ef770004bb3ee0c51adad291115afc3ff0
omrid3103 <150009701+omrid3103@users.noreply.github.com>
2024-03-15 09:13:01 +0200

    Update appbar.py

--- flet_proj/appbar.py
+++ flet_proj/appbar.py
@@ -2,52 +2,62 @@ import flet as ft
 
 
 
-def appbar(page):
+class AppBar:
+    def __init__(self, page: ft.Page):
+        self.page = page
+        self.menu = ft.NavigationDrawer(
+            controls=[
+                ft.Container(height=12),
+                ft.NavigationDrawerDestination(
+                    icon_content=ft.TextButton("Home", icon=ft.icons.HOME, icon_color=ft.colors.BLUE_300, on_click=lambda _: page.go('/home')),
+                ),
+                ft.NavigationDrawerDestination(
+                    icon_content=ft.TextButton("Sign Up", icon=ft.icons.DOOR_BACK_DOOR, icon_color=ft.colors.BLUE_300, on_click=lambda _: page.go('/sign_up_flet')),
+                ),
+                ft.NavigationDrawerDestination(
+                    icon_content=ft.TextButton("Sign In", icon=ft.icons.LOCK_OPEN, icon_color=ft.colors.BLUE_300, on_click=lambda _: page.go('/authentication_flet')),
+                ),
+                ft.NavigationDrawerDestination(
+                    icon_content=ft.TextButton("About Us", icon=ft.icons.INFO, icon_color=ft.colors.BLUE_300, on_click=lambda _: page.go('/index')),
+                ),
+            ],
 
-    menu = ft.NavigationDrawer(
-        controls=[
-            ft.Container(height=12),
-            ft.NavigationDrawerDestination(
-                icon_content=ft.TextButton("Home", icon=ft.icons.HOME, icon_color=ft.colors.BLACK, on_click=lambda _: page.go('/home')),
-            ),
-            ft.NavigationDrawerDestination(
-                icon_content=ft.TextButton("Sign Up", icon=ft.icons.DOOR_BACK_DOOR, icon_color=ft.colors.BLACK, on_click=lambda _: page.go('/sign_up_flet')),
-            ),
-            ft.NavigationDrawerDestination(
-                icon_content=ft.TextButton("Sign In", icon=ft.icons.LOCK_OPEN, icon_color=ft.colors.BLACK, on_click=lambda _: page.go('/authentication_flet')),
-            ),
-            ft.NavigationDrawerDestination(
-                icon_content=ft.TextButton("About Us", icon=ft.icons.INFO, icon_color=ft.colors.BLACK, on_click=lambda _: page.go('/index')),
-            ),
-        ],
+        )
+        self.page.drawer = self.menu
+        self.menu_text = ft.TextButton("Menu", icon=ft.icons.MENU, icon_color=ft.colors.BLACK, on_click=self.show_menu)
+        self.theme_icon = ft.IconButton(icon=ft.icons.WB_SUNNY_OUTLINED, on_click=self.switch_theme)
+        self.title_text = ft.Text("2-G2OD")
+        self.my_appbar = ft.AppBar(
+            leading=self.menu_text,
+            leading_width=40,
+            title=self.title_text,
+            color=ft.colors.BLACK,
+            center_title=True,
+            bgcolor=ft.colors.SURFACE_VARIANT,
+            actions=[
+                self.theme_icon,
+            ],
+        )
 
-    )
-    page.drawer = menu
-
-    def switch_theme(e):
-        if page.theme_mode == "dark":
-            page.theme_mode = "light"
+    def switch_theme(self, e):
+        if self.page.theme_mode == "dark":
+            self.page.theme_mode = "light"
+            self.theme_icon.icon = ft.icons.WB_SUNNY_OUTLINED
+            self.theme_icon.icon_color = ft.colors.BLACK
+            self.title_text.color = ft.colors.BLACK
+            self.menu_text.icon_color = ft.colors.BLACK
         else:
-            page.theme_mode = "dark"
-        page.update()
+            self.page.theme_mode = "dark"
+            self.theme_icon.icon = ft.icons.SHIELD_MOON_OUTLINED
+            self.theme_icon.icon_color = ft.colors.WHITE
+            self.title_text.color = ft.colors.WHITE
+            self.menu_text.icon_color = ft.colors.WHITE
+        self.page.update()
 
-    def show_menu(e):
-        menu.open = True
-        page.drawer = menu
-        page.update()
-
-    my_appbar = ft.AppBar(
-        leading=ft.TextButton("Menu", icon=ft.icons.MENU, icon_color=ft.colors.BLACK, on_click=show_menu),
-        leading_width=40,
-        title=ft.Text("Welcome"),
-        color=ft.colors.BLACK,
-        center_title=True,
-        bgcolor=ft.colors.SURFACE_VARIANT,
-        actions=[
-            ft.IconButton(ft.icons.WB_SUNNY_OUTLINED, on_click=switch_theme),
-        ],
-    )
-    return my_appbar
+    def show_menu(self, e):
+        self.menu.open = True
+        self.page.drawer = self.menu
+        self.page.update()
 
 
 
