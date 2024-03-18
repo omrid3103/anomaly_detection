8b008a4b5dfcca12c90407883f65f454e45bb953
omrid3103 <150009701+omrid3103@users.noreply.github.com>
2024-03-18 11:48:57 +0200

    Update router.py

--- flet_proj/router.py
+++ flet_proj/router.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 import flet as ft
 from flet_proj.authentication_flet import authentication_view
 from flet_proj.user_landing_page import user_landing_view
@@ -9,24 +11,27 @@ class Router:
     def __init__(self, page):
         self.page = page
         self.sign_up_controller = authentication_view(self.page, self)
-        self.sign_up_controller.submit_button.on_click = self.sign_up_controller.sign_up_button_clicked
-        self.sign_up_controller.submit_button.text = "Sign Me Up!"
-
+        # self.sign_up_controller.submit_button.on_click = self.sign_up_controller.sign_up_button_clicked
+        # self.sign_up_controller.submit_button.text = "Sign Me Up!"
         self.sign_in_controller = authentication_view(self.page, self)
-        self.sign_in_controller.submit_button.on_click = self.sign_in_controller.sign_in_button_clicked
-        self.sign_in_controller.submit_button.text = "Sign Me In!"
-
-        self.index = initial_page_view(self.page)
-        self.home = initial_page_view(self.page)
-
+        # self.sign_in_controller.submit_button.on_click = self.sign_in_controller.sign_in_button_clicked
+        # self.sign_in_controller.submit_button.text = "Sign Me In!"
+        self.index = self.initial_page
+        self.home = self.initial_page
         self.user_landing = user_landing_view(self.page)
 
         self.user_action = user_landing_view(self.page)
         self.user_action.title_text = "ACTION"
 
+        self.initial_text: Union[ft.Text, None] = None
+
+        self.routes = {}
+        self.body: Union[ft.Container, None] = None
+
+    def routes_definition(self):
         self.routes = {
-            "/index": self.index.content,
-            "/home": self.home.content,
+            "/index": self.initial_content,
+            "/home": self.initial_content,
             "/sign_up_flet": self.sign_up_controller.column,
             "/authentication_flet": self.sign_in_controller.column,
             "/user_landing_page": self.user_landing.content,
@@ -49,6 +54,26 @@ class Router:
 
         self.user_landing.update_text()
         print(self.user_landing.details)
+
+    def initial_page(self, page: ft.Page):
+        self.page = page
+        self.initial_text = ft.Text("Initial Page", size=30)
+        self.initial_content = ft.Column(
+            [
+                ft.Row(
+                    [
+                        self.initial_text
+                    ],
+                    alignment=ft.MainAxisAlignment.CENTER
+                )
+            ]
+        )
+        # page.scroll = ft.ScrollMode.HIDDEN
+        # page.auto_scroll = True
+        self.page.theme_mode = ft.ThemeMode.LIGHT
+        self.page.add(self.initial_content)
+
+        return self.page
 
     def route_change(self, route):
         self.body.content = self.routes[route.route]
