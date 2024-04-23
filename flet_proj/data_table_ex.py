0c672e938ae3779db56657878dccd6aa2bf01d75
Omri <omrid3103@gmail.com>
2024-04-23 12:46:19 +0300

    proj general

--- flet_proj/data_table_ex.py
+++ flet_proj/data_table_ex.py
@@ -8,6 +8,7 @@ class DataTable:
 
     def __init__(self, page: ft.Page, pdf_path: str = r"C:\Users\Sharon's PC\PycharmProjects\anomaly_detection\db_and_pdf_demo\client_data_table0.pdf"):
         self.page = page
+        self.page.scroll = True
         self.controller = kmc_cntrl.KMCController(pdf_path)
         self.controller.csv_to_dataframe()
         self.df: pd.DataFrame = self.controller.df
@@ -21,22 +22,18 @@ class DataTable:
         # ******************   DropDown Selection & Button   ******************************
         # =================================================================================
 
-        self.dropdown_button = ft.ElevatedButton(text="Submit", on_click=self.dropdown_button_clicked)
         self.dropdown_obj = ft.Dropdown(
             width=170,
-            options=self.dropdown_options_generation()
-            # [
-            #     ft.dropdown.Option("Red"),
-            #     ft.dropdown.Option("Green"),
-            #     ft.dropdown.Option("Blue"),
-            # ],
+            options=self.dropdown_options_generation(),
+            on_change=self.dropdown_button_clicked
         )
+        self.data_not_found = ft.Text("Data was not found...", weight="bold", size=20)
 
         # =================================================================================
         # ************************   Search TextField   ***********************************
         # =================================================================================
 
-        self.search_field = ft.TextField(label="search", width=350)
+        self.search_field = ft.TextField(label="search", width=350, on_change=self.input_changed)
 
 
         # =================================================================================
@@ -57,16 +54,18 @@ class DataTable:
             data_row_color={"hovered": "0x30FF0000"},
             divider_thickness=0,
             column_spacing=150,
+            data_row_max_height=45,
             columns=self.table_columns_generation(),
             rows=self.table_rows_generation()
         )
+        self.table_container = ft.Container
 
 
         self.search_row = ft.Row([self.dropdown_obj, self.search_field], spacing=80)
-        self.button_row = ft.Row([self.dropdown_button])
+        self.message_row = ft.Row([self.data_not_found])
         self.table_row = ft.Row([self.data_table])
 
-        self.items = [self.search_row, self.button_row, self.table_row]
+        self.items = [self.search_row, self.table_row]
         self.column = ft.Column(spacing=20, controls=self.items)
 
 
@@ -92,7 +91,7 @@ class DataTable:
             row_i_information = self.df.iloc[i].tolist()
             for j in range(self.num_cells_in_each_row):
                 cells_list.append(
-                    ft.DataCell(ft.Text(f"{row_i_information[j]}", text_align=ft.TextAlign.CENTER))
+                    ft.DataCell(ft.Text(f"{str(row_i_information[j])}", text_align=ft.TextAlign.CENTER))
                 )
             rows_list.append(
                 ft.DataRow(
@@ -105,19 +104,61 @@ class DataTable:
             cells_list = []
         return rows_list
 
+
+    # =================================================================================
+    # *******************   Dropdown Options & Button  ********************************
+    # =================================================================================
+
+
     def dropdown_options_generation(self) -> list[ft.dropdown.Option]:
         options_list = []
         for i in range(len(self.columns_names_list)):
-            options_list.append(
-                ft.dropdown.Option(f"{self.columns_names_list[i]}")
-            )
+            if isinstance(self.df.iloc[0, i], str):
+                options_list.append(
+                    ft.dropdown.Option(f"{self.columns_names_list[i]}")
+                )
         return options_list
 
     def dropdown_button_clicked(self, e):
         for k in range(len(self.columns_names_list)):
             if self.dropdown_obj.value == self.columns_names_list[k]:
-                self.search_field.hint_text = f"Search in {self.dropdown_obj.value} column..."
+                self.search_field.hint_text = f"Search in the {self.dropdown_obj.value} column..."
         self.search_field.update()
+        self.page.update()
+
+    def input_changed(self, e):
+        def cells_generation(x_c: dict[str, str]) -> list[ft.DataCell]:
+            cells_g = []
+            for n in range(self.num_cells_in_each_row):
+                cells_g.append(
+                    ft.DataCell(ft.Text(f"{str(x_c[self.columns_names_list[n]])}"))
+                )
+            return cells_g
+
+        search_name = self.search_field.value
+
+        my_filtered_df = self.df[self.df[self.columns_names_list[self.columns_names_list.index(self.dropdown_obj.value)]].str.contains(search_name)]
+        my_filter = my_filtered_df.astype(str).to_dict(orient='records')
+        self.data_table.rows = []
+
+        if not self.search_field.value == "":
+            if len(my_filter) > 0:
+                for x in my_filter:
+                    self.data_table.rows.append(
+                        ft.DataRow(
+                            cells=cells_generation(x)
+                        )
+                    )
+            else:
+                self.items.remove(self.table_row)
+                self.items.append(self.message_row)
+                self.items.append(self.table_row)
+        else:
+            if self.message_row in self.items:
+                self.items.remove(self.message_row)
+            self.data_table.rows = self.table_rows_generation()
+
+        self.data_table.update()
         self.page.update()
 
 
--- tests/testing.py
+++ tests/testing.py
@@ -1,22 +1,126 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+import flet as ft
 
-# Generate random data
-x = np.random.normal(size=100)
-y = np.random.normal(size=100)
-z = np.random.normal(size=100)
 
-# Create a 3D scatter plot with asterisk (*) shape for the points
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(x, y, z, marker='*')  # Change marker here
+def main(page: ft.Page):
+    # MY FAKE DATA
+    datax = [
+        {"name": "joe", "last": "sumidin", "age": 12},
+        {"name": "danr", "last": "sumidin", "age": 12},
+        {"name": "soedwo", "last": "sumidin", "age": 12},
+        {"name": "michele", "last": "sumidin", "age": 12},
+        {"name": "anto", "last": "sumidin", "age": 12},
+        {"name": "budi", "last": "sumidin", "age": 12},
+        {"name": "gogong", "last": "sumidin", "age": 12},
+        {"name": "urip", "last": "sumidin", "age": 12},
+        {"name": "uwur", "last": "sumidin", "age": 12},
+        {"name": "jaka sumidan", "last": "sumidin", "age": 12},
+        {"name": "pedro", "last": "sumidin", "age": 12},
 
-# Set labels and title
-ax.set_xlabel('X Label')
-ax.set_ylabel('Y Label')
-ax.set_zlabel('Z Label')
-ax.set_title('3D Scatter Plot')
+    ]
+    print(type(datax))
 
-# Show plot
-plt.show()
+    dt = ft.DataTable(
+        columns=[
+            ft.DataColumn(ft.Text("First name")),
+            ft.DataColumn(ft.Text("Last name")),
+            ft.DataColumn(ft.Text("Age")),
+        ],
+        rows=[]
+
+    )
+
+    # PUSH DATA TO DATATABLE
+    for x in datax:
+        dt.rows.append(
+            ft.DataRow(
+                cells=[
+                    ft.DataCell(ft.Text(x['name'])),
+                    ft.DataCell(ft.Text(x['last'])),
+                    ft.DataCell(ft.Text(x['age']))
+                ]
+
+            )
+
+        )
+
+    def inputsearch(e):
+        search_name = nameinput.value.lower()
+        # myfiler = list(filter(lambda x: search_name in x['name'].lower(), datax))
+
+        print("you find ", myfiler)
+        dt.rows = []
+
+        # IF NO INPUT IN YOU TEXTFIELD SEARCH
+        # THEN PUSH THE RESULT TO YOU TABLE
+        if not nameinput.value == "":
+            # AND IF LENGHT OF RESULT > 0
+            if len(myfiler) > 0:
+                datanotfound.visible = False
+                for x in myfiler:
+                    dt.rows.append(
+                        ft.DataRow(
+                            cells=[
+                                ft.DataCell(ft.Text(x['name'])),
+                                ft.DataCell(ft.Text(x['last'])),
+                                ft.DataCell(ft.Text(x['age'])),
+
+                            ]
+
+                        )
+
+                    )
+                    page.update()
+            else:
+                print("data not found")
+                datanotfound.visible = True
+                page.update()
+
+        # IF NO INPUT IN YOU TEXTFIELD
+        # THEN PUSH AGAIN DATAX IN YOU DATATABLE
+        else:
+            datanotfound.visible = False
+            page.update()
+            for x in datax:
+                dt.rows.append(
+                    ft.DataRow(
+                        cells=[
+                            ft.DataCell(ft.Text(x['name'])),
+                            ft.DataCell(ft.Text(x['last'])),
+                            ft.DataCell(ft.Text(x['age'])),
+
+                        ]
+
+                    )
+
+                )
+            page.update()
+
+    nameinput = ft.TextField(label="Search name",
+                          # IF YOU SEARCH NAME THEN RUN FUNCTION
+                          on_change=inputsearch
+
+                          )
+
+    # IF DATA NOT FOUND THE SHOW TEXT NOT FOUND
+    datanotfound = ft.Text("YOu search Not found...",
+                        weight="bold",
+                        size=20
+
+                        )
+
+    # SET DEFAULT IS FALSE
+    datanotfound.visible = False
+
+    page.add(
+        ft.Column([
+            nameinput,
+            dt,
+            datanotfound
+
+        ])
+
+    )
+
+
+ft.app(target=main)
+
